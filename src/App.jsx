77092ddd5adf0d3cc3c0f6a1d9347800444204ac
Rohan sahu <therohansahu7@gmail.com>
2025-02-09 23:40:51 +0530

    Added React flow

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Logo from '/Logo.svg'
+import { CirclePlay } from 'lucide-react';
+import { Background, Controls, ReactFlow } from '@xyflow/react';
+import './App.css'
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+function App() {
+
+
+  return (
+    <main>
+      <nav>
+        <img src={Logo} alt="Logo" />
+        <div className="nav-btns">
+          <button className='deploy-btn'>Deploy</button>
+          <button className='run-btn'> <CirclePlay/> Run</button>
+        </div>
+      </nav>
+      <section>
+      <ReactFlow nodes={initialNodes} edges={initialEdges}   >
+        <Controls />
+        <Background />
+      </ReactFlow >
+      </section>
+
+    </main>
+  )
+}
+
+export default App
